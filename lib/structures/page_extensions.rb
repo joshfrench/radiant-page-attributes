5ac22dfdd9561ace86b710c619e4473f537330ad
Josh French <josh@vitamin-j.com>
2008-10-25 14:39:52 -0400

    Page behavior and specs

--- lib/structures/page_extensions.rb
+++ lib/structures/page_extensions.rb
@@ -4,6 +4,15 @@ module Structures
     def self.included(base)
       base.class_eval do
         has_many :page_attributes
+        
+        def build_page_attributes=(*args)
+          self.page_attributes.destroy_all
+          args.compact.each do |page_attribute|
+            self.page_attributes.build(page_attribute)
+          end
+          self.page_attributes
+        end
+        
       end
     end
      
