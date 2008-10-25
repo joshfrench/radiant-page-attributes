83ec4ded1234db5a266f008edc5359805535000f
Andrew O'Brien <obrien.andrew@gmail.com>
2008-10-25 15:03:37 -0400

    Started structures

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -1,2 +1,13 @@
-class Structure < ActiveRecord::Base
+class Structure
+  def file_name
+    name.gsub(" ", '').underscore
+  end
+  
+  def partial_name
+    "_#{file_name}"
+  end
+  
+  def definition_path
+    
+  end
 end
