05e03a5407d5ad251b65ea266fd01e8966dd74cf
Andrew O'Brien <obrien.andrew@gmail.com>
2008-10-25 15:49:36 -0400

    PageAttribute#serialize! added

--- spec/models/page_attribute_spec.rb
+++ spec/models/page_attribute_spec.rb
@@ -1,18 +1,37 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 class SimpleBoolean < PageAttribute
+  def true_string
+    "Yes"
+  end
+  
+  def serialize!
+    self.value = self.value == self.true_string
+  end
 end
 
 describe PageAttribute do
   before(:each) do
-    @pa = SimpleBoolean.new(:name => "Should I?")
+    @s = SimpleBoolean.new(:name => "Should I?", :value => "Yes")
   end
 
   it "should give a file_name" do
-    @pa.file_name.should == "simple_boolean"
+    @s.file_name.should == "simple_boolean"
   end
 
   it "should give a partial_name" do
-    @pa.partial_name.should == "_simple_boolean"
+    @s.partial_name.should == "_simple_boolean"
+  end
+
+  it "should serialize the value" do
+    @s.value = "Yes"
+    @s.serialize!
+    
+    @s.value.should be_true
+    
+    @s.value = "nooooooooooo!"
+    @s.serialize!
+    
+    @s.value.should be_false
   end
 end
