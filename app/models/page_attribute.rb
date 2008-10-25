9a134098645b443680439ddf314aac3e44c77316
Josh French <josh@vitamin-j.com>
2008-10-25 15:36:41 -0400

    Simplification: remove references to structures

--- app/models/page_attribute.rb
+++ app/models/page_attribute.rb
@@ -1,7 +1,5 @@
 class PageAttribute < ActiveRecord::Base
   validates_presence_of :page_id
-  validates_presence_of :structure_id
   validates_presence_of :name
   belongs_to :page
-  belongs_to :structure
 end
