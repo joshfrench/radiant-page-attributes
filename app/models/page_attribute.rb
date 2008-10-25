da0ee7c89a66f0825bc87161dc8624b0836e0998
Andrew O'Brien <obrien.andrew@gmail.com>
2008-10-25 15:38:12 -0400

    PageAttribute is now the base class for all structures

--- app/models/page_attribute.rb
+++ app/models/page_attribute.rb
@@ -4,4 +4,12 @@ class PageAttribute < ActiveRecord::Base
   validates_presence_of :name
   belongs_to :page
   belongs_to :structure
+
+  def file_name
+    self.class.name.underscore
+  end
+  
+  def partial_name
+    "_#{file_name}"
+  end
 end
--- spec/models/page_attribute_spec.rb
+++ spec/models/page_attribute_spec.rb
@@ -1,11 +1,18 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+class SimpleBoolean < PageAttribute
+end
+
 describe PageAttribute do
   before(:each) do
-    @page_attribute = PageAttribute.new
+    @pa = SimpleBoolean.new(:name => "Should I?")
   end
 
-  it "should be valid" do
-    @page_attribute.should be_valid
+  it "should give a file_name" do
+    @pa.file_name.should == "simple_boolean"
+  end
+
+  it "should give a partial_name" do
+    @pa.partial_name.should == "_simple_boolean"
   end
 end
