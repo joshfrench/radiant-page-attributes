79aadc540e351167edf5ebf8c4e8534c564d64cf
Andrew O'Brien <obrien.andrew@gmail.com>
2008-10-25 21:31:09 -0400

    Started renaming structures to page_attributes

--- public/javascripts/admin/structures.js
+++ public/javascripts/admin/structures.js
@@ -1,12 +1,12 @@
-function toggle_add_structure_popup() {
-  var popup = $('add-structure-popup');
+function toggle_add_page_attribute_popup() {
+  var popup = $('add-page-attribute-popup');
   center(popup);
   Element.toggle(popup);
-  Field.focus($('structure-name-field'));
+  Field.focus($('page-attribute-name-field'));
 }
 
-function valid_structure_name() {
-  var stNameField = $('structure-name-field');
+function valid_page_attribute_name() {
+  var stNameField = $('page-attribute-name-field');
   var name = stNameField.value.downcase().strip();
   var result = true;
   if (name == '') {
@@ -17,24 +17,24 @@ function valid_structure_name() {
   return result;
 }
 
-function structure_loading() {
-  $('add-structure-button').disabled = true;
-  $('structure-busy').appear();
+function page_attribute_loading() {
+  $('add-page_attribute-button').disabled = true;
+  $('page-attribute-busy').appear();
 }
 
-function structure_added() {
-  var stNameField = $('structure-name-field');
-  var stIndexField = $('structure-index-field');
+function page_attribute_added() {
+  var stNameField = $('page-attribute-name-field');
+  var stIndexField = $('page-attribute-index-field');
   var index = parseInt(stIndexField.value || 0) + 1;
   var tab = 'tab-' + index;
   var caption = stNameField.value;
   var page = 'page-' + index;
 //  tabControl.addTab(tab, caption, page);
-  Element.hide('structure-part-popup');
-  Element.hide('structure-busy');
+  Element.hide('page-attribute-part-popup');
+  Element.hide('page-attribute-busy');
   stNameField.value = '';
   stIndexField.value = (index + 1).toString();
-  $('add-structure-button').disabled = false;
+  $('add-page_attribute-button').disabled = false;
   Field.focus(stNameField);
 //  tabControl.select(tab);
 }
