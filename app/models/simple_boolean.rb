e543aa8e2038417e4901fb7e9e8a1fbe3b18b903
Josh French <josh@vitamin-j.com>
2008-10-27 21:45:31 -0400

    Access value as true boolean

--- app/models/simple_boolean.rb
+++ app/models/simple_boolean.rb
@@ -1,5 +1,10 @@
 class SimpleBoolean < PageAttribute
   def serialize!
     self.value = (self.value.to_i == 1)
+    true # must return true, or before_save callback halts 
+  end
+  
+  def boolean_value
+    self.value.to_i == 1
   end
 end
