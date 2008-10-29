e170dfa2637753b6c3d4d98954abb0ef68cd8e4c
Josh French <josh@vitamin-j.com>
2008-10-28 21:19:49 -0400

    Allow assignment by base class

--- app/models/page_attribute.rb
+++ app/models/page_attribute.rb
@@ -9,17 +9,17 @@ class PageAttribute < ActiveRecord::Base
   belongs_to :page
 
   class_inheritable_accessor :storage_column
+  attr_accessor :value
   
   class << self
     def new(attributes={})
       attributes = HashWithIndifferentAccess.new(attributes)
-      value = attributes.delete(:value)
       new_record = super(attributes)
       if klass_name = attributes.delete(:class_name) and self.base_class.is_descendant_class_name?(klass_name)
         klass = klass_name.constantize
         new_record = new_record.becomes(klass)
         new_record.class_name = klass_name
-        new_record.write_attribute(klass.storage_column, value)
+        new_record.write_attribute(klass.storage_column, attributes[:value])
       end
       new_record
     end
--- spec/models/subclass_spec.rb
+++ spec/models/subclass_spec.rb
@@ -4,13 +4,23 @@ describe SimpleBoolean do
   it "should store value as boolean" do
     a = SimpleBoolean.new(:boolean_value => false)
     a.value.should be_false
-  end  
+  end
+  
+  it "should init with boolean value" do
+    a = SimpleBoolean.new(:value => true)
+    a.value.should be_true
+  end
 end
 
 describe SimpleDateTime do
   it "should store value as datetime" do
     a = SimpleDateTime.new(:datetime_value => Date.today)
     a.value.should eql(Date.today)
+  end
+  
+  it "should init with datetime value" do
+    a = SimpleDateTime.new(:value => Time.now)
+    a.value.to_s.should eql(Time.now.to_s)
   end
 end
 
@@ -19,6 +29,11 @@ describe SimpleInteger do
     a = SimpleInteger.new(:integer_value => 12345)
     a.value.should eql(12345)
   end
+  
+  it "should init with integer value" do
+    a = SimpleInteger.new(:value => 54321)
+    a.value.should eql(54321)
+  end
 end
 
 describe SimpleString do
@@ -26,11 +41,21 @@ describe SimpleString do
     a = SimpleString.new(:string_value => %(sweet harmonious biscuits))
     a.value.should eql('sweet harmonious biscuits')
   end
+  
+  it "should init with integer value" do
+    a = SimpleString.new(:value => %(sweet harmonious biscuits))
+    a.value.should eql('sweet harmonious biscuits')
+  end
 end
 
 describe SimpleText do
   it "should store value as text" do
     a = SimpleString.new(:string_value => %(sweet harmonious biscuits))
+    a.value.should eql('sweet harmonious biscuits')
+  end
+  
+  it "should init with integer value" do
+    a = SimpleString.new(:value => %(sweet harmonious biscuits))
     a.value.should eql('sweet harmonious biscuits')
   end
 end
