d2449332aef17510ce3ea1735dd9a99768243cbe
Josh French <josh@vitamin-j.com>
2008-10-28 21:18:38 -0400

    Remove attribute setter

--- lib/page_attributes/page_extensions.rb
+++ lib/page_attributes/page_extensions.rb
@@ -4,10 +4,6 @@ module PageAttributes
     def self.included(base)
       base.class_eval do
         has_many :page_attributes do
-          def []=(name,value)
-            self.build(:name => name, :value => value, :page => proxy_owner)
-          end
-          
           def [](name)
             if attr = self.find_by_name(name)
               attr.value
