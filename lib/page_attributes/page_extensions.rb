6ddb1525f31ce4ebd87dd30a37addcd190f889e8
Josh French <josh@vitamin-j.com>
2009-03-21 15:54:23 -0400

    Steal the nice PageParts pattern

--- lib/page_attributes/page_extensions.rb
+++ lib/page_attributes/page_extensions.rb
@@ -1,6 +1,6 @@
 module PageAttributes
   module PageExtensions
-    
+
     def self.included(base)
       base.class_eval do
         has_many :page_attributes, :dependent => :destroy do
@@ -10,17 +10,36 @@ module PageAttributes
             end
           end
         end
+        after_save :save_page_attributes
+        alias_method_chain :page_attributes, :pending
+        alias_method_chain :page_attributes=, :pending
       end
     end
-    
-    def build_page_attributes=(args={})
-      self.page_attributes.destroy_all
-      args.each_pair do |i,hash|
-        attr = self.page_attributes.build(hash)
-        attr.page = self
-      end
-      self.page_attributes
+      
+    def page_attributes_with_pending(reload=false)
+      @page_attrs = nil if reload
+      @page_attrs || page_attributes_without_pending(reload)
     end
-     
+
+    def page_attributes_with_pending=(collection)
+      if collection.values.all? {|item| item.is_a? PageAttribute }
+        self.page_attributes_without_pending = collection.values
+      else
+        self.updated_at_will_change!
+        @page_attrs = collection.values.map { |item| PageAttribute.new(item.merge(:page => self)) }
+      end
+    end
+
+    private
+
+      def save_page_attributes
+        if @page_attrs
+          self.page_attributes_without_pending.clear
+          @page_attrs.each {|p| self.page_attributes_without_pending << p }
+        end
+        @page_attrs = nil
+        true
+      end
+
   end
 end
